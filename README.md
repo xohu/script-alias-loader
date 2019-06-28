84da6fdabc65e0b89292dec305a23f9c9c05b032
xohus <zhuqinhu@qq.com>
2019-06-28 11:30:40 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,64 @@
+# @xohu/script-alias-loader
 
-- **`vue-cli3 alias loader`**
+> vue-cli3 的 script-alias-loader 插件
 
-  ```
-  使用此 loader 将使你能在 script 中使用 alias 定义图片路径
-  ```
+**安装**
+
+```
+cnpm install @xohu/script-alias-loader -D
+```
+
+## 说明
+```
+使用此 loader 将使你能在 script 中使用 alias别名 定义图片路径
+```
+
+## 配置
+``` js
+// vue.config.js
+module.exports = {
+    chainWebpack: config => {
+        config.module.rule('vue').use('script-alias-loader').loader('@xohu/script-alias-loader')
+    }
+}
+```
+
+## 使用
+``` js
+<template>
+  <div>
+    <img :src="img1" />
+    <div v-html="img2"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        img1: '',
+        img2: ''
+    }
+  },
+  mounted() {
+    this.$nextTick(this.init);
+  },
+  methods: {
+    init() {
+      // 这里的 @ 就是别名 alias，它指向 src 目录
+      this.img1 = `@/assets/img/logo.png`;
+      
+      this.img2 = '<img src="@/assets/img/logo.png"></img>';
+
+      this.$confirm("<img src='@/assets/img/logo.png'></img>删除？", "提示");
+    }
+  }
+}
+</script>
+```
+
+## 注意事项
+```
+不支持在路径中使用变量
+```
+  
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const alias = Object.keys(process.VUE_CLI_SERVICE.resolveWebpackConfig().resolve.alias)
 const script = /<script[\s\S]*?<\/script>/gi
-const img = /([`'"])(?:(?!\1).)*\.(png|jpe?g|gif|webp)\b(?:(?!\1).)*\1/gi
-const hasImg = /(?<=<img\s*src=)\s*[`'"]([^`'"]+)[`'"]/gi
-const start_end  = /^\s*[`'"]+|[`'"]+\s*$/g
+const content = /([`'"])(?:(?!\1).)*\.(png|jpe?g|gif|webp)\b(?:(?!\1).)*\1/gi
+const img = /(?<=<img\s*src=)\s*([`'"])([^`'"]+)\1/gi
 const isAlias = str => alias.some(v => v == str.match(/(?<=^.)[^/]+/g)[0])
 
-module.exports = function(source) {
-  return source.replace(script, $1 => $1.replace(img, $$1 => {
-      if (hasImg.test($$1)) {
-        return $$1.replace(start_end, '`').replace(hasImg, ($$$1, $$$2) => (
-          isAlias($$$1) ? `\${require("${$$$2}")}` : $$$1
+module.exports = source => (
+  source.replace(script, $1 => $1.replace(content, ($$1, $$2) => {
+      if (img.test($$1)) {
+        return $$1.replace(img, $$$1 => (
+          isAlias($$$1) ? `${$$2} + require(${$$$1}) + ${$$2}` : $$$1
         ))
       } else {
         return isAlias($$1) ? `require(${$$1})` : $$1
       }
   }))
-}
+)
